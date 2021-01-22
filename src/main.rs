1e9584a9677aa3735a7958baca08b60ce0675514
JT <mccrone@umich.edu>
2021-01-22 17:09:31 +0000

    what a mess but parsing works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,112 @@
 mod tree;
+// mod newickParser;
 
 use structopt::StructOpt;
 use std::{path, io};
 use std::io::{BufReader, BufRead, Read};
 use std::fs::File;
-use crate::tree::{TreeNode, Tree};
+// use tree::NewickParser;
+use crate::tree::{TreeNode, Tree, FixedNode};
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest_consume::{match_nodes, Error, Parser};
+
+#[derive(Parser)]
+#[grammar = "newick.pest"]
+pub struct NewickParser;
+
+type Result<T> = std::result::Result<T, Error<Rule>>;
+type Node<'i> = pest_consume::Node<'i, Rule, ()>;
+
+#[pest_consume::parser]
+impl NewickParser {
+    fn branchlength(input: Node) -> Result<f64> {
+        input.as_str()
+            .parse::<f64>()
+            // `input.error` links the error to the location in the input file where it occurred.
+            .map_err(|e| input.error(e))
+    }
+    fn length(input: Node) -> Result<f64> {
+        Ok(match_nodes!(input.into_children();
+            [branchlength(n)] =>n
+        ))
+    }
+    fn name(input:Node)->Result<String>{
+        let name = input.as_str();
+        Ok(name.to_string())
+    }
+    fn leaf(input: Node) -> Result<FixedNode> {
+        let mut tip = FixedNode::new();
+        let name = input.as_str();
+        tip.taxon = Some(name.to_string());
+        Ok(tip)
+    }
+    fn branch(input: Node) -> Result<FixedNode> {
+        let mut node: Option<FixedNode> = None;
+
+        Ok(match_nodes!(input.into_children();
+            [subtree(mut n),length(l)]=>{n.length=Some(l);n},
+            [subtree(n)]=>n
+        ))
+    }
+
+    fn branchset(input: Node) -> Result<Vec<FixedNode>> {
+        let mut children: Vec<FixedNode>=vec![];
+        Ok(match_nodes!(input.into_children();
+            [branch(child)]=>{
+            children.push(child);
+            children
+            },
+            [branch(child),branchset(siblings)]=>{
+                children.push(child);
+                for sibling in siblings{
+                    children.push(sibling);
+                }
+                children
+            }
+        ))
+    }
+    //returns a node with name and children
+    fn internal(input: Node) -> Result<FixedNode> {
+        let mut internal = FixedNode::new();
+        Ok(match_nodes!(input.into_children();
+        [branchset(children)]=>{
+            for child in children{
+                internal.children.push(Box::new(child))
+            };
+            internal
+           },
+          [branchset(children),name(n)]=>{
+             for child in children{
+                internal.children.push(Box::new(child))
+            };
+            internal.label=Some(n);
+            internal
+          }
+        ))
+    }
+
+    //Just pass the leaf or internal node back to the parent
+    fn subtree(input: Node) -> Result<FixedNode> {
+        Ok(match_nodes!(input.into_children();
+            [leaf(tip)]=>tip,
+            [internal(node)]=>node
+        ))
+    }
+
+    fn tree(input:Node)->Result<FixedNode>{
+
+        Ok(match_nodes!(input.into_children();
+            [subtree(root)] =>{root},
+            [branch(root)]=>{root}
+            ))
+    }
+
+}
+
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "command line tools for processing phylogenetic trees in rust")]
@@ -54,44 +156,26 @@ fn main() {
     // }
 
     let mut tree = Tree::new();
-    let e = tree.add_node(TreeNode::new(Some(1),None));
-    let d = tree.add_node(TreeNode::new(Some(2),Some(e)));
-    let c = tree.add_node(TreeNode::new(Some(5),Some(e)));
-    let a = tree.add_node(TreeNode::new(Some(3),Some(d)));
-    let b = tree.add_node(TreeNode::new(Some(4),Some(d)));
-
-
+    let e = tree.add_node(TreeNode::new(Some("a".to_string()), None));
+    let d = tree.add_node(TreeNode::new(Some("a".to_string()), Some(e)));
+    let c = tree.add_node(TreeNode::new(Some("a".to_string()), Some(e)));
+    let a = tree.add_node(TreeNode::new(Some("a".to_string()), Some(d)));
+    let b = tree.add_node(TreeNode::new(Some("a".to_string()), Some(d)));
 
     tree.set_root(Some(e));
-
     let mut preorder = tree.iter();
 
     preorder = tree.iter();
-    while let Some(i)= preorder.next(&tree){
+    while let Some(i) = preorder.next(&tree) {
         let node = tree.node_at(i).expect("node to exist at given index");
-        println!("{}", node.annotation.unwrap())
-        }
-
-}
-
-fn parse_input(input: Option<path::PathBuf>) -> io::Result<()> {
-    match input {
-        Some(path) => {
-            let file = File::open(path)?;
-            let reader = BufReader::new(file);
-            // TODO parse newick trees!
-            for line in reader.lines() {
-                println!("{}", line?)
-            }
-        }
-        None => {
-            println!("no file");
-            let stdin = io::stdin();
-            let mut handel = stdin.lock();
-            for line in handel.lines() {
-                println!("{}", line?)
-            }
-        }
+        println!("{}", node.taxon.as_ref().unwrap())
     }
-    Ok(())
+
+    let inputs = NewickParser::parse(Rule::tree, "((a:1,b:1),c:1);").unwrap();
+    // There should be a single root node in the parsed tree
+    println!("{}", inputs);
+    let input = inputs.single().unwrap();
+    // Consume the `Node` recursively into the final value
+    let l = NewickParser::tree(input);
+    println!("{:?}", l.unwrap());
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,27 +1,47 @@
 use std::path::Iter;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::channel;
 
 pub type TreeIndex = usize;
 
+#[derive(Debug)]
+pub struct FixedNode {
+    pub children: Vec<Box<FixedNode>>,
+    pub label: Option<String>,
+    pub taxon: Option<String>,
+    pub length: Option<f64>
+}
+
+impl FixedNode {
+    pub(crate) fn new() ->Self{
+        FixedNode {
+            children: vec![],
+            label: None,
+            taxon:None,
+            length:None
+        }
+    }
+}
+
+
 pub struct TreeNode {
-    pub annotation: Option<usize>,
+    pub taxon: Option<String>,
     pub parent: Option<TreeIndex>,
     pub first_child: Option<TreeIndex>,
     pub next_sibling: Option<TreeIndex>,
     pub previous_sibling: Option<TreeIndex>,
-
+    pub branch_length: Some(f64),
 }
 
 impl TreeNode {
-    pub fn new(annotation: Option<usize>,
+    pub fn new(annotation: Option<String>,
                parent: Option<TreeIndex>) -> Self {
-        TreeNode { annotation: annotation, parent: parent, first_child: None, next_sibling: None, previous_sibling: None }
+        TreeNode { taxon: annotation, parent: parent, first_child: None, next_sibling: None, previous_sibling: None, branch_length: None }
     }
 }
 
 pub struct Tree {
-    nodes: Vec<Option<TreeNode>>,
+    pub nodes: Vec<Option<TreeNode>>,
     root: Option<TreeIndex>,
 
 }
@@ -33,6 +53,10 @@ impl Tree {
             root: None,
         }
     }
+    pub fn from_fixed_node(root:FixedNode){
+
+    }
+
     pub fn iter(&self) -> PreorderIter {
         PreorderIter::new(self.root)
     }
@@ -43,7 +67,7 @@ impl Tree {
         let index = self.nodes.len();
         self.nodes.push(Some(node));
         let child = self.node_at(index).expect("no way we hit this");
-        if let Some(parent)=child.parent{
+        if let Some(parent) = child.parent {
             self.add_child(parent, index);
         }
         return index;
@@ -57,7 +81,6 @@ impl Tree {
             sibling.next_sibling = Some(child);
             let child_node = self.node_at_mut(child).expect("child not in tree");
             child_node.previous_sibling = Some(last_child);
-
         } else {
             let mut parent_node = self.node_at_mut(parent).expect("parent to be part of the tree");
             parent_node.first_child = Some(child);
@@ -99,7 +122,12 @@ impl Tree {
                 child = self.node_at(sibling).expect("expected sibling node to be in the tree");
             }
         }
-        return children
+        return children;
+    }
+
+    pub fn set_branchlength(&mut self, index: TreeIndex, bl: f64) {
+        let node = self.node_at_mut(index).expect("node not in tree");
+        node.branch_length = bl;
     }
 }
 
@@ -120,7 +148,7 @@ impl PreorderIter {
     pub fn next(&mut self, tree: &Tree) -> Option<TreeIndex> {
         while let Some(node_index) = self.stack.pop() {
             if let Some(node) = tree.node_at(node_index) {
-                if let Some(sibling)=node.next_sibling{
+                if let Some(sibling) = node.next_sibling {
                     self.stack.push(sibling);
                 }
                 if let Some(child) = node.first_child {
@@ -133,4 +161,92 @@ impl PreorderIter {
     }
 }
 
+
+// pub struct NewickParser {
+//     node_stack: Vec<TreeIndex>,
+//     current_node: Option<TreeIndex>,
+//     in_comment: bool,
+//     in_quote: bool,
+//     expectation: Expectation,
+//     deliminators: HashSet<&'static str>,
+//
+// }
+//
+// impl NewickParser {
+//     pub fn new() -> Self {
+//         Self {
+//             node_stack: vec![],
+//             current_node: None,
+//             in_comment: false,
+//             in_quote: false,
+//             expectation: Expectation::Non,
+//             deliminators: vec![",", ":", "[", "]", ";", ":", "(", ")", "{", "}", "\'"].into_iter().collect(),
+//         }
+//     }
+//     fn expect(&mut self, expectation: Expectation) {
+//         self.expectation = expectation;
+//     }
+//     fn clear_expectation(&mut self) {
+//         self.expectation = Expectation::Non;
+//     }
+//     pub fn parse_newick(&mut self, string: String) -> Tree {
+//         let nwk_string = string.split_word_bounds().collect::<Vec<&str>>();
+//         let mut tree = Tree::new();
+//         for token in nwk_string {
+//             if self.deliminators.contains(token) {
+//                 match token {
+//                     //TODO figure out what to do here
+//                     "(" => {
+//                         let node = TreeNode::new(None, self.current_node);
+//                         if let Some(node) = self.current_node {
+//                             self.node_stack.push(n)
+//                         }
+//                         self.current_node = Some(tree.add_node(node));
+//                         self.expect(Expectation::Taxon);
+//                     }
+//                     "," => {
+//                         let parent = self.node_stack.pop();
+//                         tree.set_parent(parent, self.current_node.unwrap());
+//                         tree.add_child(parent.unwrap(), self.current_node.unwrap());
+//                         self.expect(Expectation::Taxon);
+//                     }
+//                     ")" => {
+//                         let parent = self.node_stack.pop();
+//                         tree.set_parent(parent, self.current_node.unwrap());
+//                         tree.add_child(parent.unwrap(), self.current_node.unwrap());
+//                         self.current_node = parent;
+//                         self.expect(Expectation::Label);
+//                     }
+//                     ":" => {
+//                         self.expect(Expectation::Length)
+//                     }
+//                     _ => {}
+//                 }
+//                 println!("found {}", token)
+//             } else {
+//                 match self.expectation {
+//                     Expectation::Label => {
+//                         self.clear_expectation();
+//                     }
+//                     Expectation::Length => {
+//                         tree.set_branchlength(tree.get_node(self.current_node.unwrap()).unwrap(), token.parse().unwrap());
+//                         self.clear_expectation();
+//                     }
+//                     Expectation::Taxon => {
+//                         let taxon: Taxon = Taxon { name: token.parse().unwrap() };
+//                         tree.set_taxon(self.current_node.unwrap(), Some(taxon));
+//                         self.clear_expectation()
+//                     }
+//                     Expectation::AnnotationKey => {}
+//                     Expectation::AnnotationValue => {}
+//                     Expectation::Non => {
+//                         println!("AAHHHH got here : {}", token)
+//                     }
+//                 }
+//             }
+//         };
+//         println!("{}", tree.nodes.len());
+//         return tree;
+//     }
+// }
 
