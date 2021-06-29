d7959b4b23095f443eb60c3473670adbd35c52cf
JT <mccronejt@gmaill.com>
2021-06-29 13:22:40 +0100

    checks sampling respects lag within an introduction

--- src/commands.rs
+++ src/commands.rs
@@ -690,12 +690,40 @@ pub mod transmission_lineage {
                                 l.add_taxa(tree, node);
                             }
                         } else { // if it respects the lag
-                            let current_index = if self.respects_lag(tree, node, 0.0) { lineage_index } else { None };
-                            for child in tree.get_children(node) {
-                                self.find_lineages(tree, child, current_index);
+
+                            if self.will_be_sampled_before_lag(tree, node, 0.0) && !self.has_been_sampled_within_lag(tree, node, 0.0){
+                                let id = self.lineages.len();
+                                let mut parent_location = tree.get_annotation(parent, &self.key).unwrap();
+                                // if parent is in the same location assert it was passed up because of the
+                                //height cutoff and go back to root or first parent with location not here
+                                let parent_height = tree.get_height(parent).unwrap();
+                                while parent_location == &self.value {
+                                    parent = tree.get_parent(parent).expect("Hit the root looking for ancestor location");
+                                    parent_location = tree.get_annotation(parent, &self.key).unwrap();
+                                }
+
+                                let new_lineage = TransmissionLineage {
+                                    taxa: vec![],
+                                    tmrca: tree.get_height(node).unwrap(),
+                                    parent_tmrca: parent_height,
+                                    id,
+                                    source: parent_location.to_string(),
+                                    first_seen: 0.0,
+                                    last_seen: tree.get_height(tree.get_root().unwrap()).unwrap(),
+                                };
+                                self.lineages.push(new_lineage);
+                                trace!("adding new lineage due to gap in sampling");
+                                for child in tree.get_children(node) {
+                                    self.find_lineages(tree, child, Some(id));
+                                }
+                            }else{
+                                let current_index = if self.will_be_sampled_before_lag(tree, node, 0.0) { lineage_index } else { None };
+                                for child in tree.get_children(node) {
+                                    self.find_lineages(tree, child, current_index);
+                                }
                             }
                         };
-                    } else if tree.get_height(node).expect("nodes should have heights") >= self.cutoff &&  self.respects_lag(tree, node, 0.0) {
+                    } else if tree.get_height(node).expect("nodes should have heights") >= self.cutoff &&  self.will_be_sampled_before_lag(tree, node, 0.0) {
                         // new lineage
 
                         let id = self.lineages.len();
@@ -718,6 +746,8 @@ pub mod transmission_lineage {
                             last_seen: tree.get_height(tree.get_root().unwrap()).unwrap(),
                         };
                         self.lineages.push(new_lineage);
+                        trace!("lineage found");
+
                         if tree.is_external(node) {
                             let taxa = tree.get_taxon(node).expect("tip should have an associated taxon");
                             if !self.ignore_taxa.contains(taxa) { // if we are not ignoring this tip
@@ -742,7 +772,7 @@ pub mod transmission_lineage {
             } else {
                 //At the root
                 let annotation = tree.get_annotation(node, &self.key).unwrap();
-                if annotation == &self.value && self.respects_lag(tree, node, 0.0) {
+                if annotation == &self.value && self.will_be_sampled_before_lag(tree, node, 0.0) {
                     let id = self.lineages.len();
                     let new_lineage = TransmissionLineage {
                         taxa: vec![],
@@ -765,29 +795,52 @@ pub mod transmission_lineage {
             }
         }
 
-        fn respects_lag(&self, tree: &MutableTree, node: usize, current_lag: f64) -> bool {
+        fn will_be_sampled_before_lag(&self, tree: &MutableTree, node: usize, current_lag: f64) -> bool {
             let mut respects = false;
             if self.lag == f64::INFINITY {
                 respects = true;
-            } else if tree.get_annotation(node, &self.key).expect("node should annotation") != &self.value {
+            } else if tree.get_annotation(node, &self.key).expect("node should annotation") != &self.value || current_lag>self.lag{
                 respects = false;
-            } else if current_lag> self.lag{
-                respects =false;
             } else if tree.is_external(node) {
                 respects = true;
             }else{
                     for child in tree.get_children(node) {
                         let l = tree.get_length(child).unwrap()+current_lag;
-                        respects = respects || self.respects_lag(tree, child, l);
+                        respects = respects || self.will_be_sampled_before_lag(tree, child, l);
                         if respects {
                             break;
                         }
                     }
                 }
-
-
             respects
         }
+        // helper function to be called when checking if a new tl needs to be inserted.
+        // this makes assumptions
+        fn has_been_sampled_within_lag(&mut self, tree:&MutableTree, mut node: usize,current_lag:f64)->bool{
+            let mut respects = false;
+            if self.lag == f64::INFINITY {
+                respects = true;
+            }
+            let mut parent = tree.get_parent(node);
+            let mut new_lag = current_lag;
+            while current_lag<self.lag && parent.is_some() && !respects{
+                let parent_annotation = tree.get_annotation(parent.unwrap(),&self.key).unwrap();
+                if parent_annotation!=&self.value{
+                    break;
+                }
+                new_lag += tree.get_length(node).unwrap();
+                for child in tree.get_children(node) {
+                    if child != node {
+                        respects = respects || self.will_be_sampled_before_lag(tree, child, new_lag);
+                    }
+                }
+                node = parent.unwrap();
+               parent = tree.get_parent(node);
+            }
+
+            return respects;
+        }
+
     }
 
     pub fn run<R: std::io::Read, T: TreeImporter<R>>(mut trees: T, ignore_taxa: Option<path::PathBuf>, key: String, value: String, taxa_flag: bool, origin: Option<f64>, cutoff: Option<f64>, lag: Option<f64>) -> Result<(), Box<dyn Error>> {
@@ -853,6 +906,19 @@ pub mod transmission_lineage {
             //TODO work this into Lineage Finder
             tree.calc_node_heights();
             let mut lf = LineageFinder::new("location".to_string(), AnnotationValue::Discrete("UK".to_string()), HashSet::new(), f64::NEG_INFINITY,0.05);
+
+            lf.find_lineages(&tree, tree.get_root().unwrap(), None);
+            assert_eq!(2, lf.lineages.len());
+            assert_eq!("A", lf.lineages[0].taxa[0]);
+        }
+
+        #[test]
+        fn find_lineages_internal_lag() {
+            let s = "((A[&location=UK]:0.1,B[&location=UK]:0.1)[&location=UK]:6.1,'C'[&location=UK]:0.1)[&location=US];";
+            let mut tree = NewickImporter::read_tree(BufReader::new(s.as_bytes())).expect("error in parsing");
+            //TODO work this into Lineage Finder
+            tree.calc_node_heights();
+            let mut lf = LineageFinder::new("location".to_string(), AnnotationValue::Discrete("UK".to_string()), HashSet::new(), f64::NEG_INFINITY,1.0);
 
             lf.find_lineages(&tree, tree.get_root().unwrap(), None);
             assert_eq!(2, lf.lineages.len());
